f2d76830be52b3e5696e8e03ff0749a20e4ea48c
Camilla <milla_mendes83@hotmail.com>
2025-05-31 23:23:30 -0300

    chore: update dependencies and database schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
-
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div className="flex-col flex gap-4  items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Hello World</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
